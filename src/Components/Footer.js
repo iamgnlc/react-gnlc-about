f96e894646c4984446f08eb4b890f1be13c45b18
GNLC <gianluca.agnocchetti@gmail.com>
2018-12-30 15:33:39 +0000

    improved footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,17 +8,31 @@ import {
 } from 'reactstrap'
 
 import { IconContext } from 'react-icons'
-import { FaHeart } from 'react-icons/fa'
+import { FaHeart, FaCopyright, FaGithub, FaReact } from 'react-icons/fa'
 
 const Footer = React.memo(function Footer(props) {
   return (
     <Navbar className="footer d-flex justify-content-center justify-content-sm-between" fixed="bottom" expand="md">
-      <IconContext.Provider value={{ className: 'react-icons' }}>
-        <small className="text-muted text-center text-md-left animated fadeInUp">&copy; {(new Date()).getFullYear()} &mdash; Made with <FaHeart className="text-danger animated pulse infinite"/> and React by <b>GNLC</b></small>
-      </IconContext.Provider>
+      <small className="text-muted text-center text-md-left d-flex align-items-center animated fadeInUp">
+        <IconContext.Provider value={{ className: "mr-1" }}>
+          <FaCopyright />
+        </IconContext.Provider>
+        {(new Date()).getFullYear()} &mdash; Made with
+        <IconContext.Provider value={{ className: "text-danger animated pulse infinite mx-1" }}>
+          <FaHeart />
+        </IconContext.Provider>
+        and React
+        <IconContext.Provider value={{ className: "text-primary mx-1" }}>
+          <FaReact />
+        </IconContext.Provider>
+        &mdash; by <b className="ml-1">GNLC</b>
+      </small>
       
-      <small className="d-block animated fadeInUp">
-        (<a href={global.githubRepo} target="_blank" rel="noopener noreferrer">{global.githubRepo}</a>)
+      <small className="d-flex align-items-center animated fadeInUp">
+        <IconContext.Provider value={{ className: "mr-1" }}>
+          <FaGithub />
+        </IconContext.Provider>
+        <a href={global.githubRepo} target="_blank" rel="noopener noreferrer">{global.githubRepo}</a>
       </small>
     </Navbar>
   )

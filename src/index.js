defc2363dd8a72beaf07f106703636c8a6b4298f
GNLC <gianluca.agnocchetti@gmail.com>
2019-07-21 09:25:54 +0100

    added sentry and hotjar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
+import * as Sentry from "@sentry/browser"
+import { hotjar } from "react-hotjar"
+
+Sentry.init({
+  dsn: `https://${process.env.REACT_APP_SENTRY_KEY}@sentry.io/${process.env.REACT_APP_SENTRY_PID}`,
+})
+
+hotjar.initialize(
+  process.env.REACT_APP_HOTJAR_ID,
+  process.env.REACT_APP_HOTJAR_SV,
+)
+
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()

9898f349d9b448cd6010b132b15f0c900e229008
iamgnlc <gianluca.agnocchetti@autorama.co.uk>
2019-11-15 11:27:39 +0000

    prerender with react-snap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import registerServiceWorker from "./registerServiceWorker"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"))
-registerServiceWorker()
+// ReactDOM.render(<App />, document.getElementById("root"))
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+  registerServiceWorker();
+} else {
+  ReactDOM.render(<App />, rootElement);
+}

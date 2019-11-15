ecb29d1d8feada496a3c183f0f6e579928d9f361
GNLC <gianluca.agnocchetti@gmail.com>
2019-11-15 18:48:41 +0000

    added sentry and hotjar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
+
+import * as Sentry from "@sentry/browser"
+import { hotjar } from "react-hotjar"
+
+if (process.env.REACT_APP_SENTRY_KEY && process.env.REACT_APP_SENTRY_PID)
+  Sentry.init({
+    dsn: `https://${process.env.REACT_APP_SENTRY_KEY}@sentry.io/${process.env.REACT_APP_SENTRY_PID}`,
+  })
+
+if (process.env.REACT_APP_HOTJAR_ID && process.env.REACT_APP_HOTJAR_SV)
+  hotjar.initialize(
+    process.env.REACT_APP_HOTJAR_ID,
+    process.env.REACT_APP_HOTJAR_SV,
+  )
 
 // ReactDOM.render(<App />, document.getElementById("root"))
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")
 
 if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-  registerServiceWorker();
+  ReactDOM.hydrate(<App />, rootElement)
+  registerServiceWorker()
 } else {
-  ReactDOM.render(<App />, rootElement);
+  ReactDOM.render(<App />, rootElement)
 }
